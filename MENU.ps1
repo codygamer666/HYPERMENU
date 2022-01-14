27f2870108b081295ba4e8bb3d35d6c2f6356270
codygamer666 <96527590+codygamer666@users.noreply.github.com>
2022-01-14 10:27:21 +0100

    Update MENU.ps1
    Added UAC support for non administrator accounts

--- MENU.ps1
+++ MENU.ps1
@@ -1,4 +1,22 @@
-﻿ # Main menu, allowing user selection
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+'running with full privileges'
+ 
+ # Main menu, allowing user selection
  function Show-Menu {
      param (
          [string]$Title = 'Easy GPU-P'
